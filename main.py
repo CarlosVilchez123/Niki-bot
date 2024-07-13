aa4ed8f6480f2c1a22dd177b3bc9bd6b214af681
CarlosVilchez123 <carloslvt123@gmail.com>
2024-07-13 08:58:10 -0500

    [update] main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import telebot
 from dotenv import load_dotenv
 from threading import Thread
+import time
+from requests.exceptions import ConnectionError
 
 load_dotenv()
 
@@ -28,4 +30,13 @@ def handle_new_members(message):
 def start(message):
     bot.reply_to(message, '¡Hola! Soy Niki bot estaré escuchando todas tus peticiones.')
 
-bot.polling(none_stop=True)
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except ConnectionError:
+        print("Connection error occurred, retrying in 15 seconds...")
+        time.sleep(15)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        time.sleep(15)  # Sleep to prevent rapid retry loop
+
