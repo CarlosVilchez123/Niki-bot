b247dcc6df4b5ba23ff5dc16977ac8ed224b10a8
CarlosVilchez123 <carloslvt123@gmail.com>
2024-07-13 09:54:54 -0500

    [update] main

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 from threading import Thread
 import time
 from requests.exceptions import ConnectionError
+from flask import Flask
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
+PORT = int(os.getenv('PORT', 5000))
 
 if API_KEY is None:
     raise ValueError("No se encontró la variable de entorno 'API_KEY'.")
@@ -30,13 +32,27 @@ def handle_new_members(message):
 def start(message):
     bot.reply_to(message, '¡Hola! Soy Niki bot estaré escuchando todas tus peticiones.')
 
-while True:
-    try:
-        bot.polling(none_stop=True)
-    except ConnectionError:
-        print("Connection error occurred, retrying in 15 seconds...")
-        time.sleep(15)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        time.sleep(15)  # Sleep to prevent rapid retry loop
+def start_bot_polling():
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except ConnectionError:
+            print("Connection error occurred, retrying in 15 seconds...")
+            time.sleep(15)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            time.sleep(15)  # Sleep to prevent rapid retry loop
 
+# Start bot polling in a separate thread
+bot_thread = Thread(target=start_bot_polling)
+bot_thread.start()
+
+# Flask server setup
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT)
